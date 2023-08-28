a142a7783dbcb0d524259e61de0a9c6162219682
rabialhosen404 <rabialhosen404@gmail.com>
2023-08-28 20:31:40 +0530

    day 2

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -1,1 +1,14 @@
 let score = 33
+let score_ = 11
+// score = true 
+
+// conversion
+let score_int = Number(score_)
+let score_string = String(score_)
+score_ = Boolean(score_string)
+
+console.log(typeof score_int)
+console.log(typeof score_string)
+
+
+
