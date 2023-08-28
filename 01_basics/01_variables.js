ce4acf3562dbff8bef50f8fea8afd171ad87a788
rabialhosen404 <rabialhosen404@gmail.com>
2023-08-28 19:34:10 +0530

    first commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 144533;
+let accountEmail = "coffeAurCode@gmail.com"
+var accountPassword = "12345"
+userName  = "jhon show"
+console.table([accountId, accountEmail, accountPassword,userName])
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+let age = 19
+let isLoggend = false
+let state;
+
+// number  = 2 to power 53
+// bigint
+// string
+//boolean
+// null => standalone value
+// undefined 
+// symbol
+
+
+// object
+console.log(typeof null);// object 
+console.log(typeof undefined) // undefined
